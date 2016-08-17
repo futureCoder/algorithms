f81a7e6872624403711f0bd20edfd3ec641086d0
futureCoder <jason-dong@foxmail.com>
2016-08-17 16:21:13 +0800

    routine commit

---
+++ LintCode/爬楼梯.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    /**
+     * @param n: An integer
+     * @return: An integer
+     */
+    int climbStairs(int n) {
+    	if(n < 2)
+    		return 1;
+        int a = 1, b = 1;
+    }
+};
