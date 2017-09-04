1c09707e8df53159beac4ff5e5c4cb2a22a2002c
jasonDong <jason-dong@foxmail.com>
2017-09-04 21:10:01 +0800

    add leetcode 004
    add leetcode 004

--- newDivide/004_MedianOfTwoSortedArrays.cpp
+++ newDivide/004_MedianOfTwoSortedArrays.cpp
@@ -15,32 +15,32 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        int m = nums1.size(), n = nums2.size();
-        int mid = (m + n) >> 1;
-        if(m < mid)
-        {
-            return nums2[mid - m];
-        }
-        if(n < mid)
-        {
-            return nums1[mid - n];
-        }
+        int len1 = nums1.size(), len2 = nums2.size();
+        int k = (len1 + len2) / 2 + 1;
+        if((len1 + len2) & 0x1)
+            return find_Kth_InSortedArrays(nums1.begin(), nums2.begin(), len1, len2, k);
+        else
+            return ( find_Kth_InSortedArrays(nums1.begin(), nums2.begin(), len1, len2, k - 1) 
+                   + find_Kth_InSortedArrays(nums1.begin(), nums2.begin(), len1, len2, k ) ) / 2.0;
+    }
 
-        int find_Kth_InSortedArrays(vector<int>& nums1, vector<int>& nums2, int m, int n, int k)
-        {
-            if(m < k)
-            {
-                return nums2[n - k];
-            }
-        }
-
-        int lowwer_findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2, int m, int n)
-        {
-
-        }
-        int upper_findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2, int m, int n)
-        {
-            
-        }
+    int find_Kth_InSortedArrays(vector<int>::const_iterator pLeft, vector<int>::const_iterator pRight, int lLen, int rLen, int k)
+    {
+        if(k < 1 || k > lLen + rLen)
+            return -1;
+        if(lLen > rLen)
+            return find_Kth_InSortedArrays(pRight, pLeft, rLen, lLen, k);
+        if(0 == lLen)
+            return *(pRight + k - 1);
+        if(1 == k)
+            return std::min(*pLeft, *pRight);
+        int lMid = std::min(k >> 1, lLen);
+        int rMid = k - lMid;
+        if(*(pLeft + lMid - 1) == *(pRight + rMid - 1))
+            return *(pLeft + lMid - 1);
+        else if(*(pLeft + lMid - 1) < *(pRight + rMid - 1))
+            return find_Kth_InSortedArrays(pLeft + lMid, pRight, lLen - lMid, rLen, k - lMid);
+        else 
+            return find_Kth_InSortedArrays(pLeft, pRight + rMid, lLen, rLen - rMid, k - rMid);
     }
 };
