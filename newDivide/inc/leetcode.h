f4d4dca9bb6a2fc1e11d853c541dc80759d50c0a
jasonDong <jason-dong@foxmail.com>
2017-09-18 13:45:10 +0800

    add new files

---
+++ newDivide/inc/leetcode.h
@@ -0,0 +1,9 @@
+#ifndef _LEETCODE__H_
+#include "stdafx.h"
+namespace Solution {
+    vector<int> twoSum(vector<int>& nums, const int ignore);        //  leetcode 015_3sum
+    vector<vector<int>> threeSum(vector<int>& nums);                //  leetcode 015_3sum
+}
+
+#endif // !_LEETCODE__H_
+
---
+++ newDivide/inc/stdafx.h
@@ -0,0 +1,16 @@
+
+#ifndef __STDAFX_H__
+
+#include <stdio.h>
+#include <tchar.h>
+#include <iostream>
+#include <vector>
+#include <map>
+#include <queue>
+#include <stack>
+
+
+using namespace std;
+
+#endif // !__STDAFX_H__
+
